4c4c9cd633b757999183f4fe49ee1f4250842147
Michael <michael.stillwell@hotmail.com>
2018-02-20 14:26:23 -0600

    Finished

--- underscore.js
+++ underscore.js
@@ -306,11 +306,11 @@ const myEmployees = [
   }
 ]
 
-const myEmployeesAddresses = 0// use pluck to get addresses here.
+const myEmployeesAddresses = _.map(myEmployees, 'address');// use pluck to get addresses here.
 
 // Now we want to use pluck to get an array of ages of the employees.
 
-const myEmployeesAges = 0 // use pluck to get ages here.
+const myEmployeesAges = _.map(myEmployees, 'age'); // use pluck to get ages here.
 
 // union
 //  Union lets us take 2 arrays, and create a new array that only has 1 entry for
@@ -335,7 +335,7 @@ const breeFriendsEmails = ["red.power@ranger.com", "pikachu@gmail.com", "james@g
                             "batman@gothan.gov", "betty.white@gmail.com", "mr.giggles@gmail.com",
                             "mrs.giggles@gmail.com", "stacey@gmail.com", "brent@gmail.com", "dave@gmail.com"];
 
-const listToSendEmailsTo = 0; // Put underscore here to make list of the union of the two address.
+const listToSendEmailsTo = _.union(bobFriendsEmails, breeFriendsEmails); // Put underscore here to make list of the union of the two address.
 
 
 // intersection
@@ -349,7 +349,7 @@ const listToSendEmailsTo = 0; // Put underscore here to make list of the union o
 // Bob and Bree have decided that instead of inviting everyone they know to their
 // party.  They are instead only going to invite those people that they both know.
 
-const listOfSharedEmails = 0; // Use underscore to create the list that are in both lists
+const listOfSharedEmails = _.intersection(bobFriendsEmails, breeFriendsEmails); // Use underscore to create the list that are in both lists
 
 // groupBy
 //  Group By lets us take an array of objects, and group then into groups based
@@ -392,11 +392,23 @@ const purchases = [{"month":"February","price":37.85},{"month":"January","price"
 {"month":"April","price":56.89},{"month":"February","price":86.19},{"month":"April","price":87.99},
 {"month":"January","price":14.25},{"month":"March","price":60.80},{"month":"February","price":23.65}]
 
-const purchasesByMonth = 0; // Use groupBy to group the purchases by the month that they were made.
+const purchasesByMonth = _.groupBy(purchases, 'month'); // Use groupBy to group the purchases by the month that they were made.
 
 // Bonus Points
-const totalByMonth = 0; // Use the groupded purchasesByMonth and reduce to create a totalByMonth object.
-
+// const totalByMonth = _.map(purchasesByMonth, function(x) { 
+//   let a = [];
+//   let b = [];
+//   for ( let key in x ) {
+//     a.push(x[key].price);
+//   }
+//   a.reduce((acc, cur) => b.push(acc + cur));
+//   return b;
+//  });
+// console.log('PURSCHASES: ', purchasesByMonth);
+// console.log('TOTALS: ', totalByMonth);
+// Use the groupded purchasesByMonth and reduce to create a totalByMonth object.
+// console.log(purchasesByMonth);
+// console.log(totalByMonth);
 // memoize
 //  Memoize lets us take a function that takes a lot of time to run, and memoize
 //  results for that function, so if we run the function with the same parameters again,
@@ -432,7 +444,9 @@ let slowN = 30;
 
 let fastN = 1000;
 
-let fastFibonnaci = 0; // use memoize to create a fast fibonnaci.  Use the same
+let fastFibonnaci = _.memoize(function(x) {
+  return x < 2 ? x: fastFibonnaci(x - 1) + fastFibonnaci(x - 2);
+}); // use memoize to create a fast fibonnaci.  Use the same
 // recursve structure that the slowFibonnaci is using, but have it be memoized
 // so that it'll remeber the previous times it's been called and increase the
 
@@ -451,15 +465,17 @@ let getDeathstar = function(n){
 
 // Below we can measure the time it takes to get a return from the api call.
 
-// console.time('getDeathstar')
-// getDeathstar(9).then(e=>{
-//   console.log(e.data)
-//   console.timeEnd('getDeathstar')
-// });
+console.time('getDeathstar')
+getDeathstar(9).then(e=>{
+  console.log(e.data)
+  console.timeEnd('getDeathstar')
+});
 
 // getPersonApi `https://swapi.co/api/people/${n}`
 
-let getJedi = // Use Memoize to remeber the previous calls made to the server
+let getJedi = _.memoize(function(x) {
+  return axios.get(`https://swapi.co/api/people/${x}`);
+})// Use Memoize to remeber the previous calls made to the server
 // then compare the times for the first and second calls of both the getJedi and
 // getDeathstar functions
 // There are no unit tests for this section. But play around with the
@@ -467,22 +483,22 @@ let getJedi = // Use Memoize to remeber the previous calls made to the server
 // the time it takes for various parts of your code to run.  This can be
 // helpful in finding slow parts of your code that you want to improve.
 
-// console.time('getJedi')
-// getJedi(1).then(e=>{
-//   console.log(e.data)
-//   console.timeEnd('getJedi')
-// });
+console.time('getJedi')
+getJedi(1).then(e=>{
+  console.log(e.data)
+  console.timeEnd('getJedi')
+});
 
 setTimeout(()=>{
-  // console.time('getDeathstar')
-  // getDeathstar(9).then(e=>{
-  //   console.log(e.data)
-  //   console.timeEnd('getDeathstar')
-  // });
+  console.time('getDeathstar')
+  getDeathstar(9).then(e=>{
+    console.log(e.data)
+    console.timeEnd('getDeathstar')
+  });
 
-  // console.time('getJedi')
-  // getJedi(1).then(e=>{
-  //   console.log(e.data)
-  //   console.timeEnd('getJedi')
-  // });
+  console.time('getJedi')
+  getJedi(1).then(e=>{
+    console.log(e.data)
+    console.timeEnd('getJedi')
+  });
 }, 2000)
